7429bffff4f6e02b6547601d1482e715884fd429
Armando Canul <armanhaz173@gmail.com>
2024-09-29 08:57:36 -0600

    readme added

---
+++ README.md
@@ -0,0 +1,4 @@
+# Simple Genetic Algorithm (SGA) example
+
+In this SGA implementation, our target is generate a specific string for example:<br/>
+from initial random strings list: `['sadf', 'sejk', 'djfp', 'mxnf']`, apply SGA for found `'hola'` string.
--- example.sga.py
+++ example.sga.py
@@ -1,3 +1,9 @@
 """
     By Armando Canul
+
+    A example for simple genetic algorithm (SGA) implementation.
+    the problem consist in found the string target
+    for example:
+    found 'My Genetic Algorithm' from population size: N
+    where population is a random string list
 """
